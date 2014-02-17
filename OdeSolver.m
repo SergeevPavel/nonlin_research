0f73f2d29d096e2af64c7a9477ee5375c019235b
Mikhail <st_w@rambler.ru>
2014-02-17 21:00:31 +0400

    Runge-Kutta mwthod add

---
+++ OdeSolver.m
@@ -0,0 +1,20 @@
+function [ T, Y ] = OdeSolver( ode, tspan, y0, h )
+
+n = length(y0);
+N = floor((tspan(2) - tspan(1)) / h) + 1;
+T = linspace(tspan(1),tspan(2),N)';
+
+Y = zeros(N,n);
+k = zeros(4,n);
+
+Y(1,:) = y0;
+for i = 1:(N-1)
+	k(1,:) = h*ode(T(i), Y(i,:));
+	k(2,:) = h*ode(T(i), Y(i,:) + 1/2 * k(1,:));
+	k(3,:) = h*ode(T(i), Y(i,:) + 1/2 * k(2,:));
+	k(4,:) = h*ode(T(i), Y(i,:) + k(3,:));
+	Y(i+1,:) = Y(i,:) + (1/6)*(k(1,:) + 2*k(2,:) + 2*k(3,:) + k(4,:));
+end
+
+end
+
