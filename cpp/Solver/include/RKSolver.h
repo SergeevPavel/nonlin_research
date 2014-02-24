0cf8c41632c97e30c65994c0c4c1372f881abb75
Pavel Sergeev <sergeev.pavel.al@gmail.com>
2014-02-25 02:06:23 +0400

    update Point class

--- cpp/Solver/include/RKSolver.h
+++ cpp/Solver/include/RKSolver.h
@@ -8,12 +8,56 @@
 #ifndef RKSOLVER_H_
 #define RKSOLVER_H_
 
-struct Point
+
+#include <cstring>
+#include <iostream>
+#include <initializer_list>
+
+
+template <int dim>
+class Point
 {
-	double x1;
-	double x2;
+public:
+	Point()
+	{
+		memset(X, 0x00, sizeof(double) * dim);
+	}
+
+	Point(double i_X[dim])
+	{
+		memcpy(X, i_X, sizeof(double) * dim);
+	}
+
+	Point(std::initializer_list<double> i_X)
+	{
+		memcpy(X, i_X.begin(), sizeof(double) * dim);
+	}
+
+	void Print()
+	{
+		for (int i = 0; i < dim; i++)
+		{
+			std::cout<<X[i]<<' ';
+		}
+		std::cout<<std::endl;
+	}
+	template<int d>
+	friend std::ostream& operator<<(std::ostream& stream, Point<d> P);
+private:
+	double X[dim];
 };
 
-void Calc();
+template <int d>
+std::ostream &operator<<(std::ostream& stream, Point<d> P)
+{
+	for (int i = 0; i < d; i++)
+	{
+		std::cout<<P.X[i]<<' ';
+	}
+	std::cout<<std::endl;
+	return stream;
+}
+
+//void Calc();
 
 #endif /* RKSOLVER_H_ */
--- cpp/Solver/src/main.cpp
+++ cpp/Solver/src/main.cpp
@@ -6,10 +6,14 @@
 
 int main()
 {
+	/*
 	std::cout<<"Start"<<std::endl;
 	clock_t t = clock();
 	Calc();
 	std::cout<<"Done!"<<std::endl;
 	std::cout<<"Elapsed time:"<< (double)(clock() - t) / CLOCKS_PER_SEC<<std::endl;
+	*/
+	Point<4> p({1.0, 2.0, 3.0, 4.0});
+	std::cout<<p;
 	return 0;
 }
